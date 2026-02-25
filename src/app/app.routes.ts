f934d047334ad54e33465c019d5da5d9be0ef20a
Bvanoo <bvanoostveldt@gmail.com>
2026-02-25 10:54:07 +0100

    guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,28 +13,36 @@ import { GroupDetails } from './group-details/group-details';
 import { SecretSanta } from './secret-santa/secret-santa';
 
 export const routes: Routes = [
-    
-    {path: "home", component: Home, 
-        // canActivate: [loginGuard]
-    },
-    {path: "login", component: Login},
-    {path: "calendar", component: Calendar,
-        //  canActivate: [loginGuard]
-        },
-    {path: "signup", component: Signup},
-    {path: "randomizer", component: Randomizer},
-    {path: "profil", component: Profil},
-    {path: "groupes", component: Groupes},
-    // {path: "messenger", component: Messenger},
-    { path: 'groupes', component: Groupes,
-        //  canActivate: [loginGuard] 
-        },
-    { path: 'groupe/:id', component: GroupDetails,
-        //  canActivate: [loginGuard] 
-        },
-    {path: "nouveaumdp", component: Nouveaumdp},
-    {path: "**", redirectTo : 'home'},
-    {path: 'secret-santa', component: SecretSanta, title: 'Espace Secret - FamilyHub'},
-
-
+  {
+    path: 'home',
+    component: Home,
+    canActivate: [loginGuard],
+  },
+  { path: 'login', component: Login },
+  {
+    path: 'calendar',
+    component: Calendar,
+    canActivate: [loginGuard],
+  },
+  { path: 'signup', component: Signup },
+  { path: 'randomizer', component: Randomizer },
+  { path: 'profil', component: Profil },
+  { path: 'groupes', component: Groupes },
+  {
+    path: 'groupes',
+    component: Groupes,
+    canActivate: [loginGuard],
+  },
+  {
+    path: 'groupe/:id',
+    component: GroupDetails,
+    canActivate: [loginGuard],
+  },
+  { path: 'nouveaumdp', component: Nouveaumdp },
+  { path: '**', redirectTo: 'home' },
+  {
+    path: 'secret-santa',
+    component: SecretSanta,
+    title: 'Espace Secret - FamilyHub',
+  },
 ];
